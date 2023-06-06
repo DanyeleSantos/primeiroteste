15d987ba0947c9f07d278d5405af5a2279b0ca8f
DanyeleSantos <135742938+DanyeleSantos@users.noreply.github.com>
2023-06-06 16:27:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Uma mensagemde teste:");
+            Console.WriteLine("Estou enviando o github");
+            Console.ReadKey();
+   
+        }
+    }
+}
